2db4d203385d7fc93eb5bf9548b8cdf9bd9fef7b
alth4669 <alth4669@colorado.edu>
2019-01-31 15:40:46 -0700

    Creating practice.sh file & storing regex_practece.sh

---
+++ practice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author : Alex Thompson
+# Date: 1/31/2019
+# Script follows here
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$(($numOne + $numTwo))
+echo "the sum is : $sum"
+let prod=numOne*numTwo
+echo "The product is $prod"
+echo "File Name: $0"
+echo "Command Line Argument 1: $1"
+grep $1 $2
